96aec0e88aaf00214306e87906c62a31867f902b
Suphasith Usdonvudhikai <suphasith.usdonvudhikai@outlook.com>
2023-03-23 09:30:04 -0400

    Removed docker config in order to push to repo

--- ImagineConfigX/restart-vauto.sh
+++
@@ -1,6 +0,0 @@
-docker stack rm Vauto-Stack 
-echo "sleeping 10"
-sleep 10
-docker stack deploy -c /etc/imagine/vauto/docker-compose-vauto.yml Vauto-Stack --with-registry-auth
-docker ps -a | grep Vauto-Stack
-
